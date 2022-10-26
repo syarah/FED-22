20af9c70cae5263d2a9415afacf7eb81905214c8
Syarah <syarah@users.noreply.github.com>
2022-10-26 16:06:19 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
-console.log("hi");
+
+var deButton = document.querySelector("footer section:nth-of-type(1) button:nth-of-type(1)");
+var deUl = document.querySelector("footer section:nth-of-type(1) ul")
+
+deButton.addEventListener("click", toggleLijstje);
+
+function toggleLijstje(){ 
+    
+    deUl.classList.toggle("open");
+
+}
+
+// kan ook volgens jeffrey // arrow function / errow operator
+// deButton.addEventListener("click", () =>{
+//     deSection.classList.toggle("open");
+// })
