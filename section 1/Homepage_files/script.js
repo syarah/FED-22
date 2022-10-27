a3a1b4690e415acbb18048000a85417c063d5147
Syarah <syarah@users.noreply.github.com>
2022-10-27 14:37:58 +0200

    Add files via upload

---
+++ section 1/Homepage_files/script.js
@@ -0,0 +1,36 @@
+// JavaScript Document
+
+var deButton = document.querySelector("footer section:nth-of-type(1) button:nth-of-type(1)");
+var deUl = document.querySelector("footer section:nth-of-type(1) ul")
+
+deButton.addEventListener("click", toggleLijstje);
+
+function toggleLijstje(){ 
+    
+    deUl.classList.toggle("open");
+
+}
+
+// kan ook volgens jeffrey // arrow function / errow operator
+// deButton.addEventListener("click", () =>{
+//     deSection.classList.toggle("open");
+// })
+
+// Menu button sesamOpenDicht
+
+// Menu SesamOpen
+var menuOpenButton = document.querySelector("header nav ul li:nth-of-type(1) button:nth-of-type(1)");
+
+menuOpenButton.addEventListener("click", menuSesamOpen);
+
+function menuSesamOpen() {
+
+    var sideNav = document.querySelector("nav:nth-of-type(2)");
+    sideNav.classList.add("openNu")
+}
+
+
+//Menu SesamDicht
+
+var menuSluitButton = document.querySelector("")
+
