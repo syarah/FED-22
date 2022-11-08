d2e35272dbfab6bc7f9eb3bf1804461b1db40965
Syarah <syarah@users.noreply.github.com>
2022-11-08 14:25:32 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -15,17 +15,31 @@ function toggleLijstje(){
 }
 
 
-// Drop down extra info (werkt niet??)
+// Drop down extra info detail pagina
 var deButtonTwee = document.querySelector("main section:nth-of-type(5) button:nth-of-type(1)");
 var deUlTwee = document.querySelector("main section:nth-of-type(5) ul")
 
 deButtonTwee.addEventListener("click", toggleLijstje2);
 
 function toggleLijstje2(){ 
-    console.log('asdkjhaksdksja')
+    
     deUlTwee.classList.toggle("openNu");
 
 }
+
+var deButtonTwee = document.querySelector("main section:nth-of-type(6) button:nth-of-type(1)");
+var deUlTwee = document.querySelector("main section:nth-of-type(6) ul")
+
+deButtonTwee.addEventListener("click", toggleLijstje2);
+
+function toggleLijstje2(){ 
+    
+    deUlTwee.classList.toggle("openNu");
+
+}
+
+
+
 
 // kan ook volgens jeffrey // arrow function / errow operator
 // deButton.addEventListener("click", () =>{
