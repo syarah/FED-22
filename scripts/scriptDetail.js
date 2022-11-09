83e29212ce79e71969a12eaf93c4e32c54274c21
Syarah <syarah@users.noreply.github.com>
2022-11-09 17:56:52 +0100

    Add files via upload

---
+++ scripts/scriptDetail.js
@@ -0,0 +1,23 @@
+// Drop down extra info detail pagina
+
+var deButtonTwee = document.querySelector("main section:nth-of-type(5) button:nth-of-type(1)");
+var deUlTwee = document.querySelector("main section:nth-of-type(5) ul")
+
+deButtonTwee.addEventListener("click", toggleLijstje2);
+
+function toggleLijstje2(){ 
+    
+    deUlTwee.classList.toggle("openNu");
+
+}
+
+var deButtonTwee = document.querySelector("main section:nth-of-type(6) button:nth-of-type(1)");
+var deUlTwee = document.querySelector("main section:nth-of-type(6) ul")
+
+deButtonTwee.addEventListener("click", toggleLijstje2);
+
+function toggleLijstje2(){ 
+    
+    deUlTwee.classList.toggle("openNu");
+
+}
